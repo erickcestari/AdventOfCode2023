75e96a313cb1e838e090fdc755cee8563e791716
Erick Cestari <erickcestari03@gmail.com>
2023-12-28 11:15:06 -0300

    feat: resolving part 1 of the day 04

---
+++ day-04/part-01/src/main.rs
@@ -0,0 +1,55 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    let file = File::open("./files/input-4.txt")?;
+
+    let lines: Vec<_> = io::BufReader::new(file).lines().collect::<Result<_, _>>()?;
+
+    let mut sum = 0;
+
+    for line in lines.iter() {
+        sum += sum_numbers_adjacents_to_symbols(line);
+    }
+
+    println!("{}", sum);
+
+    Ok(())
+}
+
+fn sum_numbers_adjacents_to_symbols(line: &str) -> usize {
+    let delimiters = [':', '|'];
+    let mut parts: Vec<&str> = vec![line];
+
+    for &delimiter in &delimiters {
+        parts = parts.iter().flat_map(|&part| part.split(delimiter)).collect();
+    }
+
+    let winning_cards: Vec<i32> = parts[1]
+    .split(" ")
+    .into_iter()
+    .flat_map(|s| s.split_whitespace())
+    .filter_map(|s| s.trim().parse().ok())
+    .collect();
+
+    let my_cards: Vec<i32> = parts[2]
+    .split(" ")
+    .into_iter()
+    .flat_map(|s| s.split_whitespace())
+    .filter_map(|s| s.trim().parse().ok())
+    .collect();
+
+    let cards_point: Vec<i32> = winning_cards
+    .iter()
+    .filter(|&card| my_cards.contains(card))
+    .cloned()
+    .collect();
+
+    if cards_point.len() < 1 {
+        return 0 
+    }
+
+    let two: usize = 2;
+
+    two.pow((cards_point.len() - 1) as u32)
+}
